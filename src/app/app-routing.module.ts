cd34773df8d832c556ad2b440e4a8caeacf72f6d
BRIANCHERUIYOT <briancheruiyot@student.moringaschool.com>
2022-01-25 11:39:45 +0300

    adds check and center components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,17 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent} from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { CheckComponent } from './check/check.component';
+import { CenterComponent } from './center/center.component';
 
 
 const routes: Routes = [
   { path: 'navbar', component: NavbarComponent},
   { path: 'homepage',  component: HomepageComponent},
+  { path: 'center', component: CenterComponent},
   { path: '', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-
-
+  { path: 'check', component: CheckComponent},
 
 ];
 @NgModule({
